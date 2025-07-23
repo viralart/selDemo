1bca1be5a7142dca6355f45fc0091a16d97355f0
Viral Not QA <viral.maurya@artoon.in>
2025-07-23 17:24:04 +0530

    First basic test case added

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,9 +1,22 @@
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+//		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.artoonsolutions.com/");
+		
+		System.out.println("Title of the page is: " + driver.getTitle());
+		if(driver.getTitle().equals("Web and Application Development Company - Artoon Solutions")) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+		driver.close();
 	}
 
 }
+// This code initializes a ChromeDriver instance for Selenium WebDriver.

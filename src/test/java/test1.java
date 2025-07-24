a48cb44dd40e573d9569d553b0eba85156db8fb9
Viral Not QA <viral.maurya@artoon.in>
2025-07-24 13:09:42 +0530

    Commit

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,13 +1,20 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 public class test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-//		ChromeDriver driver = new ChromeDriver();
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.firefox.browser", "C:\\Users\\viral.maurya\\Downloads\\geckodriver-v0.36.0-win-aarch64\\geckodriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+//		WebDriver driver1 = new EdgeDriver();
+		WebDriver driver2 = new FirefoxDriver();
 		
 		driver.get("https://www.artoonsolutions.com/");
+//		driver1.get("https://www.artoonsolutions.com/");
+		driver2.get("https://www.artoonsolutions.com/");
 		
 		System.out.println("Title of the page is: " + driver.getTitle());
 		if(driver.getTitle().equals("Web and Application Development Company - Artoon Solutions")) {
@@ -16,6 +23,9 @@ public class test1 {
 			System.out.println(false);
 		}
 		driver.close();
+//		driver1.close();
+		Thread.sleep(5000);
+		driver2.close();
 	}
 
 }

b4d4a86289d6e5cbcc1481713e3ba01ba02ecebe
Viral Not QA <viral.maurya@artoon.in>
2025-07-29 18:16:35 +0530

    Commit

--- src/test/java/Locators/locatorsDemo.java
+++ src/test/java/Locators/locatorsDemo.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class locatorsDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
-		driver.get("www.google.com");
+		driver.get("https://demo.nopcommerce.com/");
 			driver.manage().window().maximize(); // Maximize the browser window
-			driver.findElement(null);
+			Thread.sleep(2000); // Wait for 2 seconds to ensure the page loads completely
 		driver.close(); // Close the browser
 	}
 

ab77a00b54dcb7f47ee7fa87660839210b1b0647
Viral Not QA <viral.maurya@artoon.in>
2025-08-27 13:15:57 +0530

    Handle Alert Demo

--- src/test/java/HandleCheckBox/HandleAlerts.java
+++ src/test/java/HandleCheckBox/HandleAlerts.java
@@ -12,13 +12,30 @@ public class HandleAlerts {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
-		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
-		Alert myalert = driver.switchTo().alert();
-		myalert.accept();
+		
+		//Normal Alert with OK button
+//		driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
+//		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+//		Alert myalert = driver.switchTo().alert();
+//		myalert.accept();
 		
 //		driver.quit();
-
+		//Confirmation Alert with OK and Cancel button
+//		driver.findElement(By.xpath("//button[normalize-space()='Click for JS Confirm']")).click();
+//		Alert myalert1 = driver.switchTo().alert();
+//		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+//		Thread.sleep(2000);
+//		myalert1.accept();
+//		myalert1.dismiss();
+		
+		//Prompt Alert with a text box and OK and Cancel button
+		driver.findElement(By.xpath("//button[normalize-space()='Click for JS Prompt']")).click();
+		Alert myalert2 = driver.switchTo().alert();
+		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+		myalert2.sendKeys("Welcome");
+		myalert2.accept();
+//		myalert2.dismiss();
+		
 	}
 
 }

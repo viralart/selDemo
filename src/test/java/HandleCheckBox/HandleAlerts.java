4ed376a32128c9a46798d0366a8c2b2f4c9a974f
Viral Not QA <viral.maurya@artoon.in>
2025-08-27 11:42:51 +0530

    Commit

---
+++ src/test/java/HandleCheckBox/HandleAlerts.java
@@ -0,0 +1,10 @@
+package HandleCheckBox;
+
+public class HandleAlerts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/HandleCheckBox/HandleCheckBoxes.java
@@ -0,0 +1,10 @@
+package HandleCheckBox;
+
+public class HandleCheckBoxes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

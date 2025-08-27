96da0432f879bf778da61c19eb80c71d02bbe70b
Viral Not QA <viral.maurya@artoon.in>
2025-08-27 12:48:14 +0530

    Handle Alert Demo

--- src/test/java/HandleCheckBox/HandleAlerts.java
+++ src/test/java/HandleCheckBox/HandleAlerts.java
@@ -1,9 +1,23 @@
 package HandleCheckBox;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandleAlerts {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
+		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+		Alert myalert = driver.switchTo().alert();
+		myalert.accept();
+		
+//		driver.quit();
 
 	}
 

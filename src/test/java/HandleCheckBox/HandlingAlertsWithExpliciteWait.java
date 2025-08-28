58d2fc8b77033dcf07f648343e270177f8e861de
Viral Not QA <viral.maurya@artoon.in>
2025-08-28 11:26:24 +0530

    Commit

---
+++ src/test/java/HandleCheckBox/HandlingAlertsWithExpliciteWait.java
@@ -0,0 +1,12 @@
+package HandleCheckBox;
+
+public class HandlingAlertsWithExpliciteWait {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ src/test/java/HandlingiFrame/HandlingFrames.java
@@ -0,0 +1,10 @@
+package HandlingiFrame;
+
+public class HandlingFrames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

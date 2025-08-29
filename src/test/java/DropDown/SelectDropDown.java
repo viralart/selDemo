7320699ea4872c9821ea0a5c05cdd51997c2f301
Viral Not QA <viral.maurya@artoon.in>
2025-08-29 11:15:53 +0530

    Commit

---
+++ src/test/java/DropDown/SelectDropDown.java
@@ -0,0 +1,10 @@
+package DropDown;
+
+public class SelectDropDown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/HandlingiFrame/HandlingFrames.java
+++ src/test/java/HandlingiFrame/HandlingFrames.java
@@ -1,10 +1,23 @@
 package HandlingiFrame;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandlingFrames {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://ui.vision/demo/webtest/frames/");
+		driver.manage().window().maximize();
+		
+		WebElement webElement = driver.findElement(By.xpath("//div[normalize-space()='Frame1']"));
+		
 	}
 
 }
